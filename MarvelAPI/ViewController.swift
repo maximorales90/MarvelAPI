c44905d6a280140fd9f3fdf406410c7381a7e1b2
maximiliano morales <maximilianocristian.morales@bbva.com>
2022-03-16 09:57:13 -0300

    Initial Commit

---
+++ MarvelAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelAPI
+//
+//  Created by Maximiliano Morales on 16/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
