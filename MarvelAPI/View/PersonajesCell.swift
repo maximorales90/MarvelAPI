d2e31d65147d2122a60fe58633e1867458dbdbef
maximiliano morales <maximilianocristian.morales@bbva.com>
2022-03-16 20:32:40 -0300

    TableView
    -Definición de la Celda

---
+++ MarvelAPI/View/PersonajesCell.swift
@@ -0,0 +1,23 @@
+//
+//  PersonajesCell.swift
+//  MarvelAPI
+//
+//  Created by Maximiliano Morales on 16/03/2022.
+//
+
+import UIKit
+
+class PersonajesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
