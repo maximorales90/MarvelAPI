92fefc8be11f7071979ccede053464014bac8180
maximiliano morales <maximilianocristian.morales@bbva.com>
2022-03-17 14:28:17 -0300

    Cell
    -Codificación de Celdas parcial

--- MarvelAPI/Controller/PersonajesViewController.swift
+++ MarvelAPI/Controller/PersonajesViewController.swift
@@ -7,21 +7,47 @@
 
 import UIKit
 
-class PersonajesViewController: UIViewController {
+class PersonajesViewController: UIViewController, UITableViewDelegate {
     
     var homeData = APIManager()
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(PersonajesCell.self, forCellReuseIdentifier: "Cell")
+        return table
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Personajes de Marvel"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
         view.backgroundColor = .systemBackground
         
         homeData.fetchPersonajes()
-    
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
 }
+
+extension PersonajesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Marvel"
+        return cell
+    }
+    
+}
+
 
 
 
--- MarvelAPI/View/PersonajesCell.swift
+++ MarvelAPI/View/PersonajesCell.swift
@@ -7,17 +7,75 @@
 
 import UIKit
 
-class PersonajesCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+class PersonsajesCellViewModel {
+    let title: String
+    let subtitle: String
+    let imageURL: URL?
+    let imageData: Data? = nil
+    
+    init(
+        title: String,
+        subtitle: String,
+        imageURL: URL?
+    ){
+        self.title = title
+        self.subtitle = subtitle
+        self.imageURL = imageURL
     }
     
+}
+
+class PersonajesCell: UITableViewCell {
+    
+    let identifier = "PersonajesCell"
+    
+    let personajeTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 25, weight: .regular)
+        return label
+    }()
+    
+    let personajeSubtitleLabel: UILabel = {
+        let subtitle = UILabel()
+        subtitle.font = .systemFont(ofSize: 18, weight: .regular)
+        return subtitle
+    }()
+    
+    let personajeImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.backgroundColor = .systemGray
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(personajeImageView)
+        contentView.addSubview(personajeTitleLabel)
+        contentView.addSubview(personajeSubtitleLabel)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    func configure(with viewModel: PersonsajesCellViewModel ){
+        personajeTitleLabel.text = viewModel.title
+        personajeSubtitleLabel.text = viewModel.subtitle
+        
+        if let data = viewModel.imageData {
+            personajeImageView.image = UIImage(data: data)
+        }
+        else{
+//            fetch
+    }
+}
 }
